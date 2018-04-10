aefc398c0a4f4fcb53eb60cfa93a165b0809a4a0
pandeNatayasa <natayasa12@gmail.com>
2018-04-10 22:34:01 +0800

    tambahan layout registrasi

--- app/src/main/java/com/example/user_pc/tugasbesar/MainActivity.java
+++ app/src/main/java/com/example/user_pc/tugasbesar/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.user_pc.tugasbesar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        register = (TextView) findViewById(R.id.register);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent regis = new Intent(getApplicationContext(),MainActivity1.class);
+                startActivity(regis);
+            }
+        });
     }
 }
