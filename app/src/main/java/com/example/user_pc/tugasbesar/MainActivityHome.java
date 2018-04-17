a4951cfd1f209a455018f866fcac723821588b14
pandeNatayasa <natayasa12@gmail.com>
2018-04-18 01:19:12 +0800

    tambahan layout daftarkan tim

--- app/src/main/java/com/example/user_pc/tugasbesar/MainActivityHome.java
+++ app/src/main/java/com/example/user_pc/tugasbesar/MainActivityHome.java
@@ -1,15 +1,20 @@
 package com.example.user_pc.tugasbesar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivityHome extends AppCompatActivity {
     private DrawerLayout mDrawer;
     private ActionBarDrawerToggle mToggle;
+    private CardView daftarkanTim;
+    final Intent[] Tekan = new Intent[1];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +25,16 @@ public class MainActivityHome extends AppCompatActivity {
         mDrawer.addDrawerListener(mToggle);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Intent to layout halaman daftarkan tim
+        daftarkanTim = (CardView)findViewById(R.id.daftarkanTim);
+        daftarkanTim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent daftartim = new Intent(getApplicationContext(),MainActivityRegisterTim.class);
+                startActivity(daftartim);
+            }
+        });
     }
 
     @Override
