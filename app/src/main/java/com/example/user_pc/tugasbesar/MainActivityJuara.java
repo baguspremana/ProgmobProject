ee9a16991328c69b0e226d161501c3021e63898f
Pande Natayasa <natayasa12@gmail.com>
2018-05-02 10:45:42 +0800

    update layout detail tim dan detail juara

--- app/src/main/java/com/example/user_pc/tugasbesar/MainActivityJuara.java
+++ app/src/main/java/com/example/user_pc/tugasbesar/MainActivityJuara.java
@@ -1,11 +1,15 @@
 package com.example.user_pc.tugasbesar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivityJuara extends AppCompatActivity {
 
+    private Button btn2015,btn2016,btn2017;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,5 +17,32 @@ public class MainActivityJuara extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        btn2015 = (Button) findViewById(R.id.btn2015);
+        btn2016 = (Button) findViewById(R.id.btn2016);
+        btn2017 = (Button) findViewById(R.id.btn2017);
+
+        btn2015.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent view_detail_juara2015 = new Intent(getApplicationContext(),MainActivityJuaraDetail.class);
+                startActivity(view_detail_juara2015);
+            }
+        });
+        btn2016.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent view_detail_juara2016 = new Intent(getApplicationContext(),MainActivityJuaraDetail.class);
+                startActivity(view_detail_juara2016);
+            }
+        });
+
+        btn2017.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent view_detail_juara2017 = new Intent(getApplicationContext(),MainActivityJuaraDetail.class);
+                startActivity(view_detail_juara2017);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/user_pc/tugasbesar/MainActivityTeam.java
+++ app/src/main/java/com/example/user_pc/tugasbesar/MainActivityTeam.java
@@ -8,15 +8,18 @@ import android.view.View;
 
 public class MainActivityTeam extends AppCompatActivity {
 
-    private CardView team1;
+    private CardView team1,team2,team3,team4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_team);
 
-        //intent to register layout
+        //intent to detail team layout
         team1 = (CardView) findViewById(R.id.team1);
+        team2 = (CardView) findViewById(R.id.team2);
+        team3 = (CardView) findViewById(R.id.team3);
+        team4 = (CardView) findViewById(R.id.team4);
 
         team1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +28,29 @@ public class MainActivityTeam extends AppCompatActivity {
                 startActivity(view_detail_team1);
             }
         });
+
+        team2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent view_detail_team2 = new Intent(getApplicationContext(),MainActivityDetailTeam.class);
+                startActivity(view_detail_team2);
+            }
+        });
+
+        team3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent view_detail_team3 = new Intent(getApplicationContext(),MainActivityDetailTeam.class);
+                startActivity(view_detail_team3);
+            }
+        });
+        team4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent view_detail_item4 = new Intent(getApplicationContext(),MainActivityDetailTeam.class);
+                startActivity(view_detail_item4);
+            }
+        });
+
     }
 }
